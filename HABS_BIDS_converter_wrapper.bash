97d18602c7683723196e036e5a10bde7706b74b0
BJ (Robert) Anderson <floydmitchell@hotmail.com>
2025-06-16 02:26:22 -0400

    Adding BIDS converter script.

---
+++ HABS_BIDS_converter_wrapper.bash
@@ -0,0 +1,17 @@
+#! /bin/env bash
+study=HABS
+BIDS_dir=${WORK}/human/${study}/${study}_BIDS/
+
+if [[ ! -d ${BIDS_dir} ]];then
+	mkdir ${BIDS_dir}
+fi
+
+cd ${WORK}/${study}/${study}_inputs/
+
+list=$(ls *fMRI_nii4D.nii.gz | cut -d 'f' -f1);
+list=$(echo $list | tr [:space:] 'X');
+list=${list//_X/ };
+for runno in $list;do
+	python3 /home/apps/human_fMRI_pipeline/convert_to_BIDS.py ${WORK}/${study}_inputs/ ${BIDS_dir} ${runno}
+done
+	
